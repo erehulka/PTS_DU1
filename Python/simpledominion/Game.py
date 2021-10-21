ff523cd487c4dd75a8bccbf6247fef58a4a7cd5e
Erik Řehulka <erik.rehulka@gmail.com>
2021-10-21 16:31:13 +0200

    Begin implementation of game class

---
+++ Python/simpledominion/Game.py
@@ -0,0 +1,16 @@
+class Game:
+
+  def __init__(self) -> None:
+    pass
+  
+  def playCard(self, handIdx: int) -> bool:
+    pass
+
+  def endPlayCardPhase(self) -> bool: 
+    pass
+
+  def buyCard(self, buyCardIdx) -> bool:
+    pass
+  
+  def endTurn(self) -> bool:
+    pass
---
+++ Python/simpledominion/SimpleDominion.py
@@ -0,0 +1,19 @@
+
+from typing import List, Tuple, Optional
+from simpledominion.SimpleDominionInterface import SimpleDominionInterface, GameState
+from simpledominion.Game import Game
+
+class SimpleDominion(SimpleDominionInterface):
+    
+  _game: Game
+
+  def playCard(self, handIdx: int) -> Optional[GameState]:
+    if self._game.playCard(handIdx):
+      pass
+  
+  def endPlayCardPhase(self) -> Optional[GameState]:
+    pass
+  def buyCard(self, buyCardIdx: int) -> Optional[GameState]:
+    pass
+  def endTurn(self) -> Optional[GameState]:
+    pass
