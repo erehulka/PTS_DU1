65565f72de6fb8c60e609892eb2b9cc508e8aa9c
Erik Řehulka <erik.rehulka@gmail.com>
2021-10-22 11:51:13 +0200

    Implementation of Game class, with TODOs.
    The rest will be done when other classes will be implemented.

--- Python/simpledominion/Game.py
+++ Python/simpledominion/Game.py
@@ -1,16 +1,44 @@
+from typing import Optional
+from simpledominion.Turn import Turn
+from simpledominion.TurnStatus import TurnStatus
+
+PLAY_PHASE = 0
+BUY_PHASE = 1
+
 class Game:
 
+  _turn: Turn
+  _phase: int
+  
   def __init__(self) -> None:
-    pass
+    self._turn = Turn(TurnStatus(1, 1, 0))
+    self._phase = PLAY_PHASE
   
   def playCard(self, handIdx: int) -> bool:
-    pass
+    if self._phase != PLAY_PHASE:
+      # Print error
+      return False
+    
+    # Play card in turn
+    self._turn.hand.play(handIdx) # TODO Handling of response
+    return True
 
   def endPlayCardPhase(self) -> bool: 
-    pass
+    if self._phase != PLAY_PHASE:
+      # Print error
+      return False
+
+    self._phase = BUY_PHASE
+    return True
 
   def buyCard(self, buyCardIdx) -> bool:
     pass
   
   def endTurn(self) -> bool:
-    pass
+    if self._phase != BUY_PHASE:
+      # Print error
+      return False
+
+    self._phase = PLAY_PHASE
+    self._turn.turnStatus = TurnStatus(1, 1, 0)
+    # TODO Not the end of the game?
