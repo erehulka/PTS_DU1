1243f7119743fbaa75f3200e4d8f78bc6805227d
Erik Řehulka <erik.rehulka@gmail.com>
2021-10-22 11:50:08 +0200

    Imlpementation of Turn class with getters and setters (@property).

--- Python/simpledominion/Turn.py
+++ Python/simpledominion/Turn.py
@@ -10,7 +10,18 @@ class Turn:
   _hand: Hand
   _deck: Deck
 
-  def __init__(self) -> None:
-    pass
+  def __init__(self, turnStatus: TurnStatus) -> None:
+    self._turnStatus = turnStatus
+    self._discardPile = DiscardPile()
+    self._deck = Deck(self._discardPile)
+    self._hand = Hand()
+
+  @property
+  def turnStatus(self) -> TurnStatus:
+    return self._turnStatus
+
+  @property
+  def hand(self) -> Hand:
+    return self._hand
 
   
