df1f0432a84dc0bd24b5f015cb667ccb92a0a2b1
Erik Řehulka <erik.rehulka@gmail.com>
2021-10-28 16:36:49 +0200

    Fix Buy Deck test.

--- Python/test/test_buydeck.py
+++ Python/test/test_buydeck.py
@@ -9,20 +9,14 @@ class TestBuyDeck(TestCase):
 
   def setUp(self):
     buyDeckFactory = BuyDeckFactory()
-    self.buydeck = buyDeckFactory.create()
+    self.buydeck = buyDeckFactory.create(GAME_CARD_TYPE_ESTATE, 10)
 
   def test_add_cards_and_get_size(self):
-    self.assertEqual(self.buydeck.isEmpty(), True)
-    self.buydeck.addCard(FakeCard(GAME_CARD_TYPE_ESTATE))
-    self.buydeck.addCard(FakeCard(GAME_CARD_TYPE_ESTATE))
-    self.buydeck.addCard(FakeCard(GAME_CARD_TYPE_ESTATE))
-    self.buydeck.addCard(FakeCard(GAME_CARD_TYPE_ESTATE))
     self.assertEqual(self.buydeck.isEmpty(), False)
-    self.buydeck.shuffleDeck()
-    self.assertEqual(len(self.buydeck._cards), 4)
-    card = self.buydeck.getTopCard()
+    self.assertEqual(self.buydeck._cardCount, 10)
+    card = self.buydeck.getCardInfo()
     self.assertEqual(card.cardType, GAME_CARD_TYPE_ESTATE)
-    self.buydeck.shuffleDeck()
-    bought = self.buydeck.buy()
+    for i in range(10):
+      bought = self.buydeck.buy()
     self.assertEqual(bought.cardType, GAME_CARD_TYPE_ESTATE)
-    self.assertEqual(len(self.buydeck._cards), 3)
+    self.assertEqual(self.buydeck.isEmpty(), True)
