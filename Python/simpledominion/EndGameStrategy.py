2f1e291b148ca287a59c9a6c14e0d770929bbf42
Erik Řehulka <erik.rehulka@gmail.com>
2021-10-29 17:39:35 +0200

    Started whole game test and fixed End Game Strategy.

--- Python/simpledominion/EndGameStrategy.py
+++ Python/simpledominion/EndGameStrategy.py
@@ -1,25 +1,32 @@
-from typing import Optional
-from simpledominion.Game import GameInterface
+from typing import List, Optional
+from simpledominion.BuyDeck import BuyDeckInterface
 
 class EndGameStrategyInterface:
 
   def isGameover(self) -> bool:
-    return False
+    pass
+
+class EndGameStrategyFactory:
+
+  def __init__(self) -> None:
+    pass
+
+  def createAtLeastNEmptyDecks(self, buyDecks: List[BuyDeckInterface], n: int) -> EndGameStrategyInterface:
+    return AtLeastNEmptyDecks(buyDecks, n)
 
 class AtLeastNEmptyDecks(EndGameStrategyInterface):
 
-  _game: GameInterface
+  _buyDecks: List[BuyDeckInterface]
   _n: int
 
-  def __init__(self, game: GameInterface, n: int) -> None:
-    self._game = game
+  def __init__(self, buyDecks: List[BuyDeckInterface], n: int) -> None:
+    self._buyDecks = buyDecks
     self._n = n
     
   def isGameover(self) -> bool:
     emptyDecks: int = 0
-    for deck in self._game.turn.buyDecks:
+    for deck in self._buyDecks:
       if deck.isEmpty():
         emptyDecks += 1
 
-    
     return emptyDecks >= self._n
